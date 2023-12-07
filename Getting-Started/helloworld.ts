03c3eed5679f5dfbc68b722c6c1bcabbb75b8a81
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2023-12-07 13:01:00 +0530

    Create helloworld.ts

---
+++ Getting-Started/helloworld.ts
@@ -0,0 +1,12 @@
+//install the node.je
+//because node.js has an module names ts -node.This helps the typescript code to compile to js code
+//install typescript-npm install -g ts-node
+//to run the ts file in node.js  use the command  ts-node filename
+//to run in the browser have hte vs code live server extension
+
+
+//Hello world:
+console.log("Hello World");
+
+//output:
+Hello World
