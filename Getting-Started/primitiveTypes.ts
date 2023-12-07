c6d7d4511d81ccddabcbcea82678a3deabe13ed8
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2023-12-07 13:05:51 +0530

    Create primitiveTypes.ts

---
+++ Getting-Started/primitiveTypes.ts
@@ -0,0 +1,11 @@
+//type annotation-primitive types
+//String
+var name:string='Rachel';
+console.log(name);
+//number
+var age:number=21;
+console.log(age);
+//boolean
+var student:boolean=true;
+console.log(typeof(student));
+console.log(student);
